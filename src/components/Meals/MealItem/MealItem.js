70ca3053e79fed903c639d590ec0b1849424cb0d
Vishal Kharade <vishalkharade@icloud.com>
2022-08-01 00:44:20 +0530

    Chnaged currency to Rs

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -4,7 +4,7 @@ import { useContext } from "react";
 import CartContext from "../../../store/cart-context";
 
 const MealItem = (props) => {
-	const price = `$${props.price.toFixed(2)}`;
+	const price = `₹${props.price.toFixed(2)}`;
 
 	const cartCtx = useContext(CartContext);
 
